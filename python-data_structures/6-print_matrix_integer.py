7c83b254c71a42a475b28e3e614a653d9fa689bc
Letho1972 <ericmarescq@gmail.com>
2023-10-03 14:52:04 +0200

    prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for ligne in matrix:
+        for element in ligne:
+            print("{:d}".format(element), end="")
+            if element != ligne[-1]:
+                print(" ", end="")
+        print()
