e9f169a827b79822763c93f0580e0a239bf6b92c
Letho1972 <ericmarescq@gmail.com>
2023-10-09 15:14:03 +0200

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""We use a private instance attribute call size"""
+
+
+class Square:
+
+    """The init method is used to initialize an objet"""
+    def __init__(self, size):
+        self.__size = size
