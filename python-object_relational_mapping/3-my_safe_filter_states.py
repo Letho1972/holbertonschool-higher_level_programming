ec1105b8b70c77a5be9af5462e5b9b3ea58e513a
Letho1972 <ericmarescq@gmail.com>
2023-11-24 14:06:47 +0100

    That is safe from MySQL injections!

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""But this time, write one that is safe from MySQL injections!"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+    )
+    cursor = database.cursor()
+
+    cursor.execute("""
+                   SELECT * FROM states
+                   WHERE name LIKE BINARY %s
+                   ORDER BY states.id
+                   """, (sys.argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    database.close()
