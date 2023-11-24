971d8660ca1808231aa628828f5f5b13355f0cbf
Letho1972 <ericmarescq@gmail.com>
2023-11-24 13:15:41 +0100

    Filter states by user input use Binary

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     )
     cursor = database.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' \
                    ORDER BY id;".format(sys.argv[4]))
 
     rows = cursor.fetchall()
