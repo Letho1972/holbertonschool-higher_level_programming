3894c8188e2f32c2d09f5ec4c9531736c6831806
Letho1972 <ericmarescq@gmail.com>
2023-11-24 15:21:41 +0100

    All cities by state use join

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all cities from the database"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cursor = database.cursor()
+
+    cursor.execute("SELECT cities.name FROM states \
+                JOIN cities ON states.id = cities.state_id \
+                WHERE states.name = %s \
+                ORDER BY cities.id;", (sys.argv[4],))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
