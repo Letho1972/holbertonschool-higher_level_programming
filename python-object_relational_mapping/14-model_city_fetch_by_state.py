1877c11497581142b40c732b7fe996d84114cfd1
Letho1972 <ericmarescq@gmail.com>
2023-11-27 15:03:58 +0100

    Cities in state to database

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,14 +20,12 @@ if __name__ == "__main__":
     soluce = (
         session.query(City, State)
         .filter(City.state_id == State.id)
-        .order_by(City.id)        
+        .order_by(City.id)
         .all()
     )
 
     for city, state in soluce:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
-    
     session.commit()
-
     session.close()
